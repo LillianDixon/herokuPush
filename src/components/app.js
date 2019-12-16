63eb405d83637e51b7ff8fc2d4c3869652217445
Lillian Dixon <dixon.lillian92@gmail.com>
2019-12-16 10:20:29 -0700

    changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 
-console.log(process.env.REACT_APP_SECRET_KEY)
+console.log(process.env.SECRET_KEY)
 
-// const MY_SECRET_KEY = process.env.MY_SECRET_KEY
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>This is my heroku project</h1>
-        <h2>{process.env.REACT_APP_SECRET_KEY}</h2>
+        <h2>{process.env.SECRET_KEY}</h2>
       </div>
     );
   }
